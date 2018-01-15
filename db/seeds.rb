fc5f60368f3326ed7af7f2918385a09a08d3df28
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-15 16:33:55 -0700

    Generate Airport model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# 12 busieset US Airports
+Airport.create(code: 'ATL')
+Airport.create(code: 'LAX')
+Airport.create(code: 'ORD')
+Airport.create(code: 'DFW')
+Airport.create(code: 'JFK')
+Airport.create(code: 'DEN')
+Airport.create(code: 'SFO')
+Airport.create(code: 'LAS')
+Airport.create(code: 'CLT')
+Airport.create(code: 'SEA')
+Airport.create(code: 'PHX')
+Airport.create(code: 'MIA')
