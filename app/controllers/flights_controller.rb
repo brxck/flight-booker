7d29c885ba3c3376c5f38014f641dc843586a9ac
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-15 21:52:49 -0700

    Add search result feed.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,5 +4,9 @@ class FlightsController < ApplicationController
     @flight = Flight.new
     @origins = Flight.all.map{ |x| [x.origin.code, x.origin.id] }
     @destinations = Flight.all.map{ |x| [x.destination.code, x.destination.id] }
+
+    # Search results
+    @flights = Flight.where(origin_id:      params[:origin_id],
+                            destination_id: params[:destination_id])
   end
 end
