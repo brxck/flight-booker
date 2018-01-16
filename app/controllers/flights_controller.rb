810834894fece6ab511e51f0f914d7ddac7f0ff7
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-16 12:08:58 -0700

    Update search results.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -8,6 +8,7 @@ class FlightsController < ApplicationController
     if params[:origin_id] && params[:destination_id]
       @flights = Flight.where(origin_id:      params[:origin_id],
                               destination_id: params[:destination_id])
+      @flights = @flights.paginate(page: params[:page], per_page: 10)
     else
       @flights = Flight.all.paginate(page: params[:page], per_page: 10)
     end
