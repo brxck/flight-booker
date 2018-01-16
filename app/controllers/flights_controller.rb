2fea1a7f8369473bd4763802326c3ba48dbea188
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-15 21:08:56 -0700

    Add search form.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,8 @@
 class FlightsController < ApplicationController
-  def index
+  def search
+    # Options for select
     @flight = Flight.new
+    @origins = Flight.all.map{ |x| [x.origin.code, x.origin.id] }
+    @destinations = Flight.all.map{ |x| [x.destination.code, x.destination.id] }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'flights/index'
-
-  root 'flights#index'
+  root 'flights#search'
 end
